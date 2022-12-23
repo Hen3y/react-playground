108ae6b659da22e64d25ea329a90b95e9b3aa821
Hen3y <hanyukun0112@outlook.com>
2022-12-23 21:43:37 +0800

    add Portal

--- src/components/Portal.tsx
+++ src/components/Portal.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { FC, PropsWithChildren } from 'react'
+import { createPortal } from 'react-dom'
 
-export const Portal = () => {
-  return (
-    <div>Portal</div>
-  )
+interface Props {
+  container?: Element
+}
+
+export const Portal: FC<PropsWithChildren<Props>> = ({ children, container }) => {
+  const portalRoot = container || document.getElementById('portal-root') as HTMLElement
+  
+  return createPortal(children, portalRoot)
 }
